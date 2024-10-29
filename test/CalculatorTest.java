9cc9382ad697296da79a543e2898376168af3d84
edpop450 <popeduard450@gmail.com>
2024-10-29 21:25:59 +0200

    Selenium WebDriver

--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -5,70 +5,71 @@ public class CalculatorTest {
     Calculator c;
 
     @BeforeAll
-    public static void classSetup(){
-        System.out.println("Prepara testele din class");
-
+    public static void classSetup() {
+        System.out.println("Preparing all the tests from the class.");
     }
 
-
     @BeforeEach
-    public void testSetup(){
-        System.out.println("Ruleaza setup dupa fiecare test");
+    public void testSetup() {
+        System.out.println("The method runs before each test!");
         c = new Calculator();
     }
 
     @Test
-    public void testAddition01(){
-        double result = c.compute(-3, 4, "+");
-        Assertions.assertEquals(1, result, "Addition failed");
+    public void testAddition01() {
+        double result = c.compute(3,4 , "+");
+        Assertions.assertEquals(7, result,"Addition fail");
     }
 
     @Test
-    public void testAddition02(){
-        Assertions.assertEquals(14.5, c.compute(10, 4.5, "+"),"Addition failed");
+    public void testAddition02() {
+        Assertions.assertEquals(14.5, c.compute(10, 4.5, "+") , "Addition fail");
     }
 
-    @Test()
-    public void testInvalidOperator(){
+    @Test
+    public void testInvalidOperator() {
         IllegalArgumentException ex = Assertions.assertThrows(IllegalArgumentException.class, () ->
         {
-            c.compute(5,7,"--");
-        },"Operator not valid");
-
+            c.compute(5, 7, "ABC");
+        }, "Operator not valid!");
     }
 
-    @Test()
-    public void testInvalidDivisionBy0(){
+    @Test
+    public void testDivisionByZero() {
         IllegalArgumentException ex = Assertions.assertThrows(IllegalArgumentException.class, () ->
         {
-            c.compute(5,0,"/");
-        },"Imparti la zero dilau");
-
+            c.compute(5, 0, "/");
+        }, "Operator not valid!");
     }
 
     @Test
-    public void testSubtraction01(){
-        Assertions.assertEquals(10,c.compute(5,-5,"-"),"Subtraction failed.");
+    public void testSubtraction01() {
+        Assertions.assertEquals(10, c.compute(5, -5, "-"), "Subtraction failed.");
     }
 
     @Test
-    public void testMultiplication(){
-        Assertions.assertEquals(10,c.compute(2,5,"*"),"Multiplication failed.");
+    public void testMultiplication01() {
+        Assertions.assertEquals(14, c.compute(7, 2,"*"), "Multiplication failed.");
     }
 
     @Test
-    public void testSqrt(){
-        Assertions.assertEquals(Math.sqrt(2),c.compute(2,2,"SQRT"),"Sqrt failed.");
+    public void testSquareRoot01() {
+        Assertions.assertEquals(8, c.compute(64, 0, "SQRT"), "SQRT failed.");
+    }
+
+    @Test
+    public void testSquareRoot02(){
+        Assertions.assertEquals(Math.sqrt(2), c.compute(2, 0, "SQRT"), "SQRT failed.");
     }
 
     @AfterEach
-    public void cleanTest(){
-        System.out.println("Clean after the test.");
+    public void cleanTest() {
+        System.out.println("Cleanup after the test.");
     }
 
     @AfterAll
     public static void cleanClass(){
-        System.out.println("Clean at the end.");
+        System.out.println("Cleanup at the end.");
     }
 
 }
